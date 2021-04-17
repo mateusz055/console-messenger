ae8d2fe25c6c8b576750160f4b95509d64cb236f
mateusz055 <mateusziskra591@gmail.com>
2021-04-17 11:58:58 +0200

    firstcommit

---
+++ console-messenger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_messenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
